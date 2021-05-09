ccdf257c4064e38e5c3dda5d20a9be9faad60d42
unsworthme <23306050+ultramario1998@users.noreply.github.com>
2021-05-08 18:21:15 -0600

    fixed some bugs

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+from server import startServer
+from client import startClient
 
-
-selection = input("Press 1 to send a request, or 2 to receive. ")
+selection = input("Press 1 to start the server, or 2 to start the client. ")
 if selection == '1':
-	print("asdf")
 	ip = input("Please input the desired IP address, or leave blank to attempt localhost: ")
 	if not ip:
 		ip = '127.0.0.1'
-	port = input("Please input the desired port, or leave blank to attempt port 80: ")
+	port = input("Please input the desired port, or leave blank to attempt port 9999: ")
 	if not port:
-		port = '80'
-	address = (ip,port)
+		port = 9999
+	else:
+		port = int(port)
+	if not isinstance(port, int):
+		print("Invalid port!")
+	startServer(ip, port)
+elif selection == '2':
+	startClient()
+else:
+	print("Invalid selection!")
